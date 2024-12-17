711913eae04ea2312787600f148bb63cbef9ce20
+ <jacqueline.trapp18@gmail.com>
2024-12-17 12:56:08 -0500

    lab complete

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,12 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    to_visit = [self.root]
+
+    while len(to_visit) > 0:
+      node = to_visit.pop(0)
+
+      if node["id"] == id:
+        return node
+
+      to_visit = node["children"] + to_visit
